f95cc42c3f5bf93d910e191cc8a6d2704d4e79e5
Francisco <francisco.castillo@keyin.com>
2024-08-05 15:36:01 -0230

    update

---
+++ src/Buyer.java
@@ -0,0 +1,6 @@
+
+public class Buyer extends User {
+    public Buyer(String username, String password, String email) {
+        super(username, password, email, "buyer");
+    }
+}
---
+++ src/Seller.java
@@ -0,0 +1,6 @@
+
+public class Seller extends User {
+    public Seller(String username, String password, String email) {
+        super(username, password, email, "seller");
+    }
+}
